298e119a5bdb6d77f9b67449485e47c7de0d1ea5
LAPTOP-O3MJRTK1\ashle <ashley.aditya@gmail.com>
2022-05-10 13:49:28 +1000

    learn styled-components complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { css, ThemeProvider } from "styled-components";
+
+// using themes -> import ThemeProvider from styled components
+const theme = {
+  primary: "blue",
+  secondary: "red",
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const HeadingComponent = styled.h1`
+  font-family: sans-serif;
+`;
+
+const Button = styled.button`
+  ${"" /* Passing "isValid" props when calling button component */}
+  ${
+    "" /* color: ${(props) => (props.isValid ? "white" : "black")};
+  background: ${(props) => (props.isValid ? "blue" : "yellow")}; */
+  }
+
+  ${
+    "" /* You can also do the above in a simpler way instead of checking isValid for each style property. 
+    Import css from styled-components */
+  }
+
+  ${(props) =>
+    props.isValid
+      ? css`
+          color: white;
+          background: blue;
+        `
+      : css`
+          color: black;
+          background: yellow;
+        `}
+
+  &:hover {
+    background: ${(props) => props.theme.secondary};
+  }
+`;
+
+const Input = styled.input`
+  padding: 5px;
+  border-radius: 4px;
+  margin: 10px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <HeadingComponent>Styled Components</HeadingComponent>
+        <Input placeholder="usename" />
+        <Input placeholder="password" />
+        <Button isValid={true}>Login</Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
